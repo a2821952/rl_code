9a04dfad5de73ded3b5da656b18b3e0a6aae66fa
a2821952 <793557653@qq.com>
2019-12-11 10:29:28 +0800

    describe

--- mle_torch.py
+++ mle_torch.py
@@ -25,23 +25,23 @@ sample = np.array([ 1.,  1.,  0.,  1.,  1.,  1.,  0.,  1.,  1.,  1.,  1.,  1.,  
         1.,  1.,  1.,  0.,  1.,  0.,  1.,  1.,  1.,  1.,  0.,  0.,  1.,
         0.,  1.,  1.,  1.,  1.,  1.,  1.,  0.,  1.,  1.,  1.,  0.,  1.,
         1.,  1.,  1.,  0.,  1.,  1.,  1.,  0.,  0.,  0.,  1.,  1.,  1.,
-        1.,  0.,  1.,  0.,  1.])
+        1.,  0.,  1.,  0.,  1.])#原始伯努利分布结果 p = 0.725
 
 print(np.mean(sample))
 
 
-x = Variable(torch.from_numpy(sample)).type(torch.FloatTensor)
-p = Variable(torch.rand(1), requires_grad=True)
+x = Variable(torch.from_numpy(sample)).type(torch.FloatTensor) #数据转化为tensor
+p = Variable(torch.rand(1), requires_grad=True)  #目标为估计正确的p
 
 
 
-learning_rate = 0.0002
+learning_rate = 0.0002  #学习率
 for t in range(1000):
-    NLL = -torch.sum(torch.log(x*p + (1-x)*(1-p)) )
-    NLL.backward()
+    NLL = -torch.sum(torch.log(x*p + (1-x)*(1-p)) )  #使用似然函数作为loss
+    NLL.backward()  #根据loss函数反向传播
 
     if t % 100 == 0:
-        print("loglik  =", NLL.data.numpy(), "p =", p.data.numpy(), "dL/dp = ", p.grad.data.numpy())
+        print("loglik  =", NLL.data.numpy(), "p =", p.data.numpy(), "dL/dp = ", p.grad.data.numpy())  #每100次打印一次结果
     
-    p.data -= learning_rate * p.grad.data
-    p.grad.data.zero_()
+    p.data -= learning_rate * p.grad.data  #根据梯度调整p 
+    p.grad.data.zero_()  #清除累计的梯度
